9fa9f3126efab9f1dcd1f39c79a52f788cb53eb1
lUCI <lusindisontanjana@gmail.com>
2022-04-05 18:41:28 +0200

    Add data to js, dynamic functions to popup-window

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+const data = {
+	story: {
+		title: "Multi-Post Stories",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">css</li>',
+		'<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/portfolio.jpg"
+	},
+	project1: {
+		title: "Profesional Art Printing Data",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-1.jpg"
+	},
+	project2: {
+		title: "Data Dashboard Healthcare",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-2.jpg"
+	},
+	project3: {
+		title: "Website Protfolio",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-3.jpg"
+	},
+	project4: {
+		title: "Profesional Art Printing Data More",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-4.jpg"
+	},
+	project5: {
+		title: "Data Dashboard Healthcare",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-5.jpg"
+	},
+	project6: {
+		title: "Website Protfolio",
+		text:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+		lang: ['<li class="pop-up__languages--item">html</li>',
+		'<li class="pop-up__languages--item">Bootstrap</li>',
+		'<li class="pop-up__languages--item">Ruby</li>'],
+		image: "./img/project-6.jpg"
+	}
+}
+
+
 let modal = document.getElementById("modal");
 
 let btn = document.getElementById("modal-btn");
@@ -38,7 +99,10 @@ popUp.forEach(element => {
 		if (event.target) {
 			popUpModal.style.display = "block";
 			let uniqId= event.target.id;
-	
+			document.querySelector('.pop-up__heading').innerHTML = data[uniqId].title;
+			document.querySelector('.pop-up__paragraph').innerHTML = data[uniqId].text;
+			document.querySelector('.pop-up__languages').innerHTML = data[uniqId].lang;
+			document.querySelector('.popUpImage').setAttribute('src', data[uniqId].image)
 		}
 		
 	}
@@ -49,5 +113,3 @@ closePopUp.onclick = function(event) {
     popUpModal.style.display = "none";
   }
 }
-
-console.log(project.project1)
